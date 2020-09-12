acb0ee6bb8d6abf7cce5d96c442b72fad61e4fc6
Guigso <gui.soliveira@hotmail.com>
2020-09-12 12:59:57 -0300

    Adding Contentful and removing react-contentful

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,6 +1,6 @@
-import { ContentfulClient } from "react-contentful";
+import { createClient } from "contentful";
 
-const client = ContentfulClient({
+const client = createClient({
   space: process.env.REACT_APP_CONTENTFUL_SPACE || "",
   environment: process.env.REACT_APP_CONTENTFUL_ENVIRONMENT, // defaults to 'master' if not set
   accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN || "",
